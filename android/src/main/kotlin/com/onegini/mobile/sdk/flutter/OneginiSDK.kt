53623dc7da17bd599f384765a8f51cc7149a069d
stanislav <stanislav.fedosov@develocraft.com>
2021-03-11 13:11:15 +0200

    refactored OneginiSDK.kt

--- android/src/main/kotlin/com/onegini/mobile/sdk/flutter/OneginiSDK.kt
+++ android/src/main/kotlin/com/onegini/mobile/sdk/flutter/OneginiSDK.kt
@@ -10,64 +10,59 @@ import com.onegini.mobile.sdk.flutter.handlers.*
 import java.util.concurrent.TimeUnit
 
 
-
 class OneginiSDK {
 
-    companion object{
-        private var oneginiClientConfigModel: OneginiClientConfigModel? = null
-        private var oneginiSecurityController : Class<*>? = null
-        private var httpConnectionTimeout : Long = 5
-        private var httpReadTimeout : Long = 20
-        private var oneginiCustomIdentityProviders = mutableListOf<OneginiCustomIdentityProvider>()
+    companion object {
 
-
-        fun setOneginiClientConfigModel(configModel: OneginiClientConfigModel){
-            oneginiClientConfigModel = configModel
-        }
-
-        fun setSecurityController(securityController: Class<*>){
-            oneginiSecurityController = securityController
-        }
-
-        fun setConnectionTimeout(connectionTimeout : Long){
-            httpConnectionTimeout = connectionTimeout
-        }
-
-        fun setReadTimeout(readTimeout : Long){
-            httpReadTimeout = readTimeout
+        fun init(context: Context,
+                 oneginiConfigModel: OneginiClientConfigModel,
+                 securityController: Class<*>? = null,
+                 connectionTimeout: Long = 5,
+                 readTimeout: Long = 25,
+                 oneginiCustomIdentityProviders: MutableList<OneginiCustomIdentityProvider>
+                 = mutableListOf()) {
+            val oneginiSDK = OneginiSDK()
+            oneginiSDK.httpConnectionTimeout = connectionTimeout
+            oneginiSDK.httpReadTimeout = readTimeout
+            oneginiSDK.oneginiClientConfigModel = oneginiConfigModel
+            oneginiSDK.oneginiSecurityController = securityController
+            oneginiSDK.oneginiCustomIdentityProviders = oneginiCustomIdentityProviders
+            oneginiSDK.buildSDK(context)
         }
 
         fun getOneginiClient(context: Context): OneginiClient? {
             var oneginiClient = OneginiClient.getInstance()
             if (oneginiClient == null) {
-                oneginiClient = buildSDK(context)
+                oneginiClient = OneginiSDK().buildSDK(context)
             }
             return oneginiClient
         }
 
-        fun addCustomIdentityProvider(oneginiCustomIdentityProvider: OneginiCustomIdentityProvider){
-            oneginiCustomIdentityProviders.add(oneginiCustomIdentityProvider)
-        }
-
-        private fun buildSDK(context: Context): OneginiClient? {
-            if(oneginiClientConfigModel == null) throw Exception("OneginiClientConfigModel must be not null!")
-            val applicationContext = context.applicationContext ?: return null
-            val registrationRequestHandler = RegistrationRequestHandler(applicationContext)
-            val fingerprintRequestHandler = FingerprintAuthenticationRequestHandler(applicationContext)
-            val pinAuthenticationRequestHandler = PinAuthenticationRequestHandler()
-            val createPinRequestHandler = CreatePinRequestHandler(applicationContext)
-            val mobileAuthWithOtpRequestHandler = MobileAuthOtpRequestHandler()
-            val clientBuilder = OneginiClientBuilder(applicationContext, createPinRequestHandler, pinAuthenticationRequestHandler) // handlers for optional functionalities
-                    .setBrowserRegistrationRequestHandler(registrationRequestHandler)
-                    .setFingerprintAuthenticationRequestHandler(fingerprintRequestHandler)
-                    .setHttpConnectTimeout(TimeUnit.SECONDS.toMillis(httpConnectionTimeout).toInt())
-                    .setHttpReadTimeout(TimeUnit.SECONDS.toMillis(httpReadTimeout).toInt())
-                    .setMobileAuthWithOtpRequestHandler(mobileAuthWithOtpRequestHandler)
-                    .setSecurityController(oneginiSecurityController)
-                    .setConfigModel(oneginiClientConfigModel)
-            oneginiCustomIdentityProviders.map { clientBuilder.addCustomIdentityProvider(it) }
-            return clientBuilder.build()
-        }
     }
 
+    var oneginiClientConfigModel: OneginiClientConfigModel? = null
+    var oneginiSecurityController: Class<*>? = null
+    var httpConnectionTimeout: Long = 5
+    var httpReadTimeout: Long = 20
+    var oneginiCustomIdentityProviders = mutableListOf<OneginiCustomIdentityProvider>()
+
+    private fun buildSDK(context: Context): OneginiClient? {
+        if (oneginiClientConfigModel == null) throw Exception("OneginiClientConfigModel must be not null!")
+        val applicationContext = context.applicationContext ?: return null
+        val registrationRequestHandler = RegistrationRequestHandler(applicationContext)
+        val fingerprintRequestHandler = FingerprintAuthenticationRequestHandler(applicationContext)
+        val pinAuthenticationRequestHandler = PinAuthenticationRequestHandler()
+        val createPinRequestHandler = CreatePinRequestHandler(applicationContext)
+        val mobileAuthWithOtpRequestHandler = MobileAuthOtpRequestHandler()
+        val clientBuilder = OneginiClientBuilder(applicationContext, createPinRequestHandler, pinAuthenticationRequestHandler) // handlers for optional functionalities
+                .setBrowserRegistrationRequestHandler(registrationRequestHandler)
+                .setFingerprintAuthenticationRequestHandler(fingerprintRequestHandler)
+                .setHttpConnectTimeout(TimeUnit.SECONDS.toMillis(httpConnectionTimeout).toInt())
+                .setHttpReadTimeout(TimeUnit.SECONDS.toMillis(httpReadTimeout).toInt())
+                .setMobileAuthWithOtpRequestHandler(mobileAuthWithOtpRequestHandler)
+                .setSecurityController(oneginiSecurityController)
+                .setConfigModel(oneginiClientConfigModel)
+        oneginiCustomIdentityProviders.map { clientBuilder.addCustomIdentityProvider(it) }
+        return clientBuilder.build()
+    }
 }
