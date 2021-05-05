a29d7cdf72e466f95b385c143186257443f1c289
User <venko_132@ukr.net>
2021-05-05 19:51:29 +0300

    .

--- example/ios/Pods/Target Support Files/Pods-OneginiTests/Pods-OneginiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OneginiTests : NSObject
-@end
-@implementation PodsDummy_Pods_OneginiTests
-@end
--- example/ios/Pods/Target Support Files/onegini/onegini-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_onegini : NSObject
-@end
-@implementation PodsDummy_onegini
-@end
