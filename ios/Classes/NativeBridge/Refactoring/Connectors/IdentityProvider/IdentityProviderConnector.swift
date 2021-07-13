3a17fd329b489784d0645ba049c6f54a04dcb228
User <venko_132@ukr.net>
2021-07-13 16:40:20 +0300

    XCFormat apply

--- ios/Classes/NativeBridge/Refactoring/Connectors/IdentityProvider/IdentityProviderConnector.swift
+++ ios/Classes/NativeBridge/Refactoring/Connectors/IdentityProvider/IdentityProviderConnector.swift
@@ -1,6 +1,6 @@
+import Flutter
 import Foundation
 import OneginiSDKiOS
-import Flutter
 
 protocol IdentityProviderConnectorProtocol {
     func getIdentityProviders(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) -> Void
@@ -8,13 +8,14 @@ protocol IdentityProviderConnectorProtocol {
 
 class IdentityProviderConnector: IdentityProviderConnectorProtocol {
     private(set) var identityProviderWrapper: IdentityProviderWrapperProtocol
-    
+
     init(wrapper: IdentityProviderWrapperProtocol = IdentityProviderWrapper()) {
-        self.identityProviderWrapper = wrapper
+        identityProviderWrapper = wrapper
     }
-    
+
     func getIdentityProviders(_ call: FlutterMethodCall, _ result: @escaping FlutterResult) {
-        let list = self.identityProviderWrapper.identityProviders()
+        Logger.log("getIdentityProviders", sender: self)
+        let list = identityProviderWrapper.identityProviders()
         let data = String.stringify(json: list.toDict())
         result(data)
     }
@@ -22,13 +23,13 @@ class IdentityProviderConnector: IdentityProviderConnectorProtocol {
 
 extension Array where Element == ONGIdentityProvider {
     func toDict() -> [[String: Any]] {
-        let jsonData = self.compactMap { (identityProvider) -> [String: Any]? in
+        let jsonData = compactMap { (identityProvider) -> [String: Any]? in
             var data = [String: Any]()
             data[Constants.Parameters.id] = identityProvider.identifier
             data[Constants.Parameters.name] = identityProvider.name
             return data
         }
-        
+
         return jsonData
     }
 }
--- ios/Classes/NativeBridge/Refactoring/Connectors/Main/MainAppConnectorFactory.swift
+++ ios/Classes/NativeBridge/Refactoring/Connectors/Main/MainAppConnectorFactory.swift
@@ -5,7 +5,7 @@ protocol MainAppConnectorFactoryInterface {
     var resourcesConnector: ResourcesConnectorProtocol { get set}
     var disconnectConnector: DisconnectConnectorProtocol { get set}
     var identityProviderConnector: IdentityProviderConnectorProtocol { get}
-    var userProfilesConnector: UserProfilesConnectorProtocol { get}
+    var userProfilesConnector: UserProfilesConnectorProtocol { get }
 }
 
 class DefaultMainConnectorFactory: MainAppConnectorFactoryInterface {
