026f60e3d86b324809acdef8f53057aea3b6ab8d
stanislav <stanislav.fedosov@develocraft.com>
2021-10-05 14:02:21 +0300

    add chrome custom tabs

--- android/src/main/kotlin/com/onegini/mobile/sdk/flutter/useCases/HandleRegisteredUrlUseCase.kt
+++ android/src/main/kotlin/com/onegini/mobile/sdk/flutter/useCases/HandleRegisteredUrlUseCase.kt
@@ -3,26 +3,25 @@ package com.onegini.mobile.sdk.flutter.useCases
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import com.onegini.mobile.sdk.flutter.activity.ActivityWebView
+import androidx.browser.customtabs.CustomTabsIntent
 import io.flutter.plugin.common.MethodCall
 
 class HandleRegisteredUrlUseCase {
     operator fun invoke(call: MethodCall, context: Context) {
         val url = call.argument<String>("url") ?: ""
         val isInAppBrowser = call.argument<Int>("type")
-        val intent = prepareIntentBasedOnType(isInAppBrowser, context, url)
-        context.startActivity(intent)
-    }
-
-    private fun prepareIntentBasedOnType(type: Int?, context: Context, url: String): Intent {
-        val intent: Intent = if (type == null || type == 0) {
-            Intent(context, ActivityWebView::class.java).putExtra("url", url)
-        } else {
+        if (isInAppBrowser == null || isInAppBrowser == 0){
+            val customTabsIntent = CustomTabsIntent.Builder().build()
+            customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+            customTabsIntent.launchUrl(context, Uri.parse(url))
+        }else {
             val uri = Uri.parse(url)
-            Intent(Intent.ACTION_VIEW, uri)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+            context.startActivity(intent)
         }
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-        return intent
+
     }
 }
