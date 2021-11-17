5a48ee66b50798ca7567efed121380e26acc2591
stanislav <stanislav.fedosov@develocraft.com>
2021-11-17 13:41:54 +0200

    Changes after review

---
+++ android/src/test/java/com/onegini/mobile/sdk/FingerprintAcceptAuthenticationUseCaseTests.kt
@@ -0,0 +1,39 @@
+package com.onegini.mobile.sdk
+
+import com.onegini.mobile.sdk.android.handlers.request.callback.OneginiFingerprintCallback
+import com.onegini.mobile.sdk.flutter.OneginiSDK
+import com.onegini.mobile.sdk.flutter.handlers.FingerprintAuthenticationRequestHandler
+import com.onegini.mobile.sdk.flutter.useCases.FingerprintAcceptAuthenticationUseCase
+import io.flutter.plugin.common.MethodCall
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class FingerprintAcceptAuthenticationUseCaseTests {
+
+    @Mock
+    lateinit var callMock: MethodCall
+
+    @Mock
+    lateinit var oneginiSDKMock: OneginiSDK
+
+    @Mock
+    lateinit var fingerprintAuthenticationRequestHandlerMock: FingerprintAuthenticationRequestHandler
+
+    @Mock
+    lateinit var oneginiFingerprintCallbackMock: OneginiFingerprintCallback
+
+    @Test
+    fun `should call acceptAuthenticationRequest`() {
+        whenever(oneginiSDKMock.getFingerprintAuthenticationRequestHandler()).thenReturn(fingerprintAuthenticationRequestHandlerMock)
+        whenever(fingerprintAuthenticationRequestHandlerMock.fingerprintCallback).thenReturn(oneginiFingerprintCallbackMock)
+
+        FingerprintAcceptAuthenticationUseCase(oneginiSDKMock)()
+
+        verify(oneginiFingerprintCallbackMock).acceptAuthenticationRequest()
+    }
+}
