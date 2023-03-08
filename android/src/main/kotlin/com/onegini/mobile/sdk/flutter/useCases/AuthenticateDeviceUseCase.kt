2392bb1b6dc0af28199c4b892b238eb0c28e1d72
Archifer <pacoxavierpaco@hotmail.nl>
2023-03-08 10:50:28 +0100

    FP-20 Update authenticatedevice use case to pigeon

--- android/src/main/kotlin/com/onegini/mobile/sdk/flutter/useCases/AuthenticateDeviceUseCase.kt
+++ android/src/main/kotlin/com/onegini/mobile/sdk/flutter/useCases/AuthenticateDeviceUseCase.kt
@@ -4,27 +4,30 @@ import com.onegini.mobile.sdk.android.handlers.OneginiDeviceAuthenticationHandle
 import com.onegini.mobile.sdk.android.handlers.error.OneginiDeviceAuthenticationError
 import com.onegini.mobile.sdk.flutter.OneginiSDK
 import com.onegini.mobile.sdk.flutter.helpers.SdkError
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class AuthenticateDeviceUseCase @Inject constructor(private val oneginiSDK: OneginiSDK) {
-    operator fun invoke(call: MethodCall, result: MethodChannel.Result) {
-        val scope = call.argument<ArrayList<String>>("scope")
-        oneginiSDK.oneginiClient.deviceClient.authenticateDevice(scope?.toArray(arrayOfNulls<String>(scope.size)), object : OneginiDeviceAuthenticationHandler {
-            override fun onSuccess() {
-                result.success(true)
-            }
+  operator fun invoke(scopes: List<String>?, callback: (Result<Unit>) -> Unit) {
+    val sdkScopes: Array<String>? = scopes?.toTypedArray()
 
-            override fun onError(error: OneginiDeviceAuthenticationError) {
-                SdkError(
-                    code = error.errorType,
-                    message = error.message
-                ).flutterError(result)
-            }
-        }
+    oneginiSDK.oneginiClient.deviceClient.authenticateDevice(sdkScopes, object : OneginiDeviceAuthenticationHandler {
+      override fun onSuccess() {
+        callback(Result.success(Unit))
+      }
+
+      override fun onError(error: OneginiDeviceAuthenticationError) {
+        callback(
+          Result.failure(
+            SdkError(
+              code = error.errorType,
+              message = error.message
+            ).pigeonError()
+          )
         )
+      }
     }
+    )
+  }
 }
