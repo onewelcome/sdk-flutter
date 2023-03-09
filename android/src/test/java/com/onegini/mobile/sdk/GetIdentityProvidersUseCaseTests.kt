2e519aac28ad9d19e8dc6492cafaa59b01d82764
Archifer <pacoxavierpaco@hotmail.nl>
2023-03-09 11:50:33 +0100

    fp-20 update GetIdentityProvidersUseCaseTests.kt for pigeon

--- android/src/test/java/com/onegini/mobile/sdk/GetIdentityProvidersUseCaseTests.kt
+++ android/src/test/java/com/onegini/mobile/sdk/GetIdentityProvidersUseCaseTests.kt
@@ -1,63 +1,71 @@
 package com.onegini.mobile.sdk
 
-import com.google.gson.Gson
 import com.onegini.mobile.sdk.android.model.OneginiIdentityProvider
+import com.onegini.mobile.sdk.flutter.OneWelcomeWrapperErrors
 import com.onegini.mobile.sdk.flutter.OneginiSDK
+import com.onegini.mobile.sdk.flutter.pigeonPlugin.OWIdentityProvider
 import com.onegini.mobile.sdk.flutter.useCases.GetIdentityProvidersUseCase
-import io.flutter.plugin.common.MethodChannel
+import junit.framework.Assert.fail
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Answers
 import org.mockito.Mock
-import org.mockito.Spy
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class GetIdentityProvidersUseCaseTests {
 
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    lateinit var oneginiSdk: OneginiSDK
-    
-    @Mock
-    lateinit var oneginiIdentityProviderFirstMock: OneginiIdentityProvider
+  @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+  lateinit var oneginiSdk: OneginiSDK
 
-    @Mock
-    lateinit var oneginiIdentityProviderSecondMock: OneginiIdentityProvider
+  @Mock
+  lateinit var oneginiIdentityProviderFirstMock: OneginiIdentityProvider
 
-    @Spy
-    lateinit var resultSpy: MethodChannel.Result
+  @Mock
+  lateinit var oneginiIdentityProviderSecondMock: OneginiIdentityProvider
 
-    lateinit var getIdentityProvidersUseCase: GetIdentityProvidersUseCase
-    @Before
-    fun attach() {
-        getIdentityProvidersUseCase = GetIdentityProvidersUseCase(oneginiSdk)
+  lateinit var getIdentityProvidersUseCase: GetIdentityProvidersUseCase
+
+  @Before
+  fun attach() {
+    getIdentityProvidersUseCase = GetIdentityProvidersUseCase(oneginiSdk)
+  }
+
+  @Test
+  fun `When the sdk returns an empty set, Then it should return an empty list`() {
+    whenever(oneginiSdk.oneginiClient.userClient.identityProviders).thenReturn(emptySet())
+
+    val result = getIdentityProvidersUseCase()
+
+    Assert.assertEquals(result.getOrNull(), mutableListOf<List<OWIdentityProvider>>())
+  }
+
+  @Test
+  fun `When the user has available identity providers, Then it should return list of identity providers`() {
+    whenever(oneginiSdk.oneginiClient.userClient.identityProviders).thenReturn(
+      setOf(
+        oneginiIdentityProviderFirstMock,
+        oneginiIdentityProviderSecondMock
+      )
+    )
+    whenever(oneginiIdentityProviderFirstMock.id).thenReturn("firstId")
+    whenever(oneginiIdentityProviderFirstMock.name).thenReturn("firstName")
+    whenever(oneginiIdentityProviderSecondMock.id).thenReturn("secondId")
+    whenever(oneginiIdentityProviderSecondMock.name).thenReturn("secondName")
+
+    val result = getIdentityProvidersUseCase()
+
+    when (val identityProviders = result.getOrNull()) {
+      is List<OWIdentityProvider> -> {
+        Assert.assertEquals(identityProviders[0].id, "firstId")
+        Assert.assertEquals(identityProviders[0].name, "firstName")
+        Assert.assertEquals(identityProviders[1].id, "secondId")
+        Assert.assertEquals(identityProviders[1].name, "secondName")
+      }
+      else -> fail(OneWelcomeWrapperErrors.UNEXPECTED_ERROR_TYPE.message)
     }
-
-    @Test
-    fun `should return empty list when sdk return empty set`() {
-        whenever(oneginiSdk.oneginiClient.userClient.identityProviders).thenReturn(emptySet())
-
-        getIdentityProvidersUseCase()
-
-        val expectedResult = Gson().toJson(emptyArray<Map<String, String>>())
-        verify(resultSpy).success(expectedResult)
-    }
-
-    @Test
-    fun `should return list of identity providers when user have available identity providers`() {
-        whenever(oneginiSdk.oneginiClient.userClient.identityProviders).thenReturn(setOf(oneginiIdentityProviderFirstMock, oneginiIdentityProviderSecondMock))
-        whenever(oneginiIdentityProviderFirstMock.id).thenReturn("firstId")
-        whenever(oneginiIdentityProviderFirstMock.name).thenReturn("firstName")
-        whenever(oneginiIdentityProviderSecondMock.id).thenReturn("secondId")
-        whenever(oneginiIdentityProviderSecondMock.name).thenReturn("secondName")
-
-        getIdentityProvidersUseCase()
-
-        val expectedArray = arrayOf(mapOf("id" to "firstId", "name" to "firstName"), mapOf("id" to "secondId", "name" to "secondName"))
-        val expectedResult = Gson().toJson(expectedArray)
-        verify(resultSpy).success(expectedResult)
-    }
+  }
 }
