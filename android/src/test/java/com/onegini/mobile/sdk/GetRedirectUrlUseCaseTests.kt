49c82f37a1b9851f14fc01da1c148fb19bfbd728
Archifer <pacoxavierpaco@hotmail.nl>
2023-03-09 11:53:46 +0100

    fp-20 update GetRedirectUrlUseCaseTests.kt

--- android/src/test/java/com/onegini/mobile/sdk/GetRedirectUrlUseCaseTests.kt
+++ android/src/test/java/com/onegini/mobile/sdk/GetRedirectUrlUseCaseTests.kt
@@ -4,54 +4,50 @@ import com.onegini.mobile.sdk.android.client.OneginiClient
 import com.onegini.mobile.sdk.android.model.OneginiClientConfigModel
 import com.onegini.mobile.sdk.flutter.OneginiSDK
 import com.onegini.mobile.sdk.flutter.useCases.GetRedirectUrlUseCase
-import io.flutter.plugin.common.MethodChannel
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Answers
 import org.mockito.Mock
-import org.mockito.Spy
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class GetRedirectUrlUseCaseTests {
 
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    lateinit var oneginiSdk: OneginiSDK
+  @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+  lateinit var oneginiSdk: OneginiSDK
 
-    @Mock
-    lateinit var clientMock: OneginiClient
+  @Mock
+  lateinit var clientMock: OneginiClient
 
-    @Mock
-    lateinit var oneginiClientConfigModelMock: OneginiClientConfigModel
+  @Mock
+  lateinit var oneginiClientConfigModelMock: OneginiClientConfigModel
 
-    @Spy
-    lateinit var resultSpy: MethodChannel.Result
+  lateinit var getRedirectUrlUseCase: GetRedirectUrlUseCase
 
-    lateinit var getRedirectUrlUseCase: GetRedirectUrlUseCase
-    @Before
-    fun attach() {
-        getRedirectUrlUseCase = GetRedirectUrlUseCase(oneginiSdk)
-        whenever(oneginiSdk.oneginiClient.configModel).thenReturn(oneginiClientConfigModelMock)
-    }
+  @Before
+  fun attach() {
+    getRedirectUrlUseCase = GetRedirectUrlUseCase(oneginiSdk)
+    whenever(oneginiSdk.oneginiClient.configModel).thenReturn(oneginiClientConfigModelMock)
+  }
 
-    @Test
-    fun `When redirectUri is an empty string, Then call success with empty string`() {
-        whenever(oneginiClientConfigModelMock.redirectUri).thenReturn("")
+  @Test
+  fun `When redirectUri is an empty string, Then call success with empty string`() {
+    whenever(oneginiClientConfigModelMock.redirectUri).thenReturn("")
 
-        getRedirectUrlUseCase(resultSpy)
+    val result = getRedirectUrlUseCase()
 
-        verify(resultSpy).success("")
-    }
+    Assert.assertEquals(result.getOrNull(), "")
+  }
 
-    @Test
-    fun `When redirectUri is a non-empty string, Then call sucess with that string`() {
-        whenever(oneginiClientConfigModelMock.redirectUri).thenReturn("http://test.com")
+  @Test
+  fun `When redirectUri is a non-empty string, Then call sucess with that string`() {
+    whenever(oneginiClientConfigModelMock.redirectUri).thenReturn("http://test.com")
 
-        getRedirectUrlUseCase(resultSpy)
+    val result = getRedirectUrlUseCase()
 
-        verify(resultSpy).success("http://test.com")
-    }
+    Assert.assertEquals(result.getOrNull(), "http://test.com")
+  }
 }
