4e8d7468f78e1e34a0c8e3bb2e3a5ae22914c3bc
Michiel Vrins <michiel_678@hotmail.com>
2023-03-16 11:25:15 +0100

    FP-20: Remove redundant catch block

--- ios/Classes/NativeBridge/Handlers/PinHandler.swift
+++ ios/Classes/NativeBridge/Handlers/PinHandler.swift
@@ -66,7 +66,7 @@ class PinHandler: NSObject {
 }
 
 //MARK: -
-extension PinHandler: PinConnectorToPinHandler{
+extension PinHandler: PinConnectorToPinHandler {
     func handleFlowUpdate(_ flow: PinFlow, _ error: SdkError?, receiver: PinHandlerToReceiverProtocol) {
         if(self.flow == nil){
             self.flow = flow
@@ -214,8 +214,6 @@ extension PinHandler: ONGChangePinDelegate {
 
         if error.code == ONGGenericError.actionCancelled.rawValue {
             changePinCompletion?(.failure(FlutterError(.changingPinCancelled)))
-        } else if error.code == ONGGenericError.userDeregistered.rawValue {
-            changePinCompletion?(.failure(FlutterError(mappedError)))
         } else {
             changePinCompletion?(.failure(FlutterError(mappedError)))
         }
