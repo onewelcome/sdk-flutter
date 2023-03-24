406c54ad82a271cdf4bbacc0c99e2cd6d0ae5993
Michiel Vrins <michiel_678@hotmail.com>
2023-03-24 10:10:45 +0100

    FP-62: Update tests for SSO

--- android/src/test/java/com/onegini/mobile/sdk/GetAppToWebSingleSignOnUseCaseTests.kt
+++ android/src/test/java/com/onegini/mobile/sdk/GetAppToWebSingleSignOnUseCaseTests.kt
@@ -3,15 +3,20 @@ package com.onegini.mobile.sdk
 import android.net.Uri
 import com.google.gson.Gson
 import com.onegini.mobile.sdk.android.handlers.OneginiAppToWebSingleSignOnHandler
+import com.onegini.mobile.sdk.android.handlers.OneginiPinValidationHandler
 import com.onegini.mobile.sdk.android.handlers.error.OneginiAppToWebSingleSignOnError
 import com.onegini.mobile.sdk.android.model.OneginiAppToWebSingleSignOn
 import com.onegini.mobile.sdk.flutter.OneWelcomeWrapperErrors.*
 import com.onegini.mobile.sdk.flutter.OneginiSDK
+import com.onegini.mobile.sdk.flutter.SdkErrorAssert
 import com.onegini.mobile.sdk.flutter.facade.UriFacade
+import com.onegini.mobile.sdk.flutter.pigeonPlugin.FlutterError
+import com.onegini.mobile.sdk.flutter.pigeonPlugin.OWAppToWebSingleSignOn
 import com.onegini.mobile.sdk.flutter.useCases.GetAppToWebSingleSignOnUseCase
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import junit.framework.TestCase.assertEquals
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,10 +36,8 @@ class GetAppToWebSingleSignOnUseCaseTests {
     lateinit var oneginiSdk: OneginiSDK
 
     @Mock
-    lateinit var resultMock: MethodChannel.Result
+    lateinit var callbackMock: (Result<OWAppToWebSingleSignOn>) -> Unit
 
-    @Mock
-    lateinit var callMock: MethodCall
 
     @Mock
     private lateinit var uriFacade: UriFacade
@@ -60,52 +63,36 @@ class GetAppToWebSingleSignOnUseCaseTests {
     }
 
     @Test
-    fun `When GetAppToWebSingleSignOn is called without a url argument, Then should fail with URL_CANT_BE_NULL error`() {
-        whenCalledWithNullUrl()
-
-        getAppToWebSingleSignOnUseCase(callMock, resultMock)
-
-        val message = URL_CANT_BE_NULL.message
-        verify(resultMock).error(eq(URL_CANT_BE_NULL.code.toString()), eq(message), any())
-    }
-
-    @Test
-    fun `When oginini getAppToWebSingleSignOn calls onSuccess on the handler, Then promise should resolve with a map containing the content from the result`() {
-        whenCalledWithUrl()
+    fun `When oginini getAppToWebSingleSignOn calls onSuccess on the handler, Then promise should resolve with a OWAppToWebSingleSignOn with the token and url`() {
         mockParseUri(correctUri)
         mockSingleSignOnObject()
         whenever(oneginiSdk.oneginiClient.userClient.getAppToWebSingleSignOn(any(), any())).thenAnswer {
             it.getArgument<OneginiAppToWebSingleSignOnHandler>(1).onSuccess(oneginiAppToWebSingleSignOn)
         }
 
-        getAppToWebSingleSignOnUseCase(callMock, resultMock)
+        getAppToWebSingleSignOnUseCase(correctUri, callbackMock)
 
-        val argumentCaptor = argumentCaptor<String>()
-        verify(resultMock).success(argumentCaptor.capture())
-        // This will be reworked after FP-20 when we actually send the objects
-        val expectedResult = Gson().toJson(
-            mapOf(
-                "token" to oneginiAppToWebSingleSignOn.token,
-                "redirectUrl" to oneginiAppToWebSingleSignOn.redirectUrl.toString()
-            )
-        )
-        assertEquals(argumentCaptor.firstValue, expectedResult)
-
+        argumentCaptor<Result<OWAppToWebSingleSignOn>>().apply {
+            verify(callbackMock).invoke(capture())
+            Assert.assertEquals(firstValue.getOrNull()?.token, oneginiAppToWebSingleSignOn.token)
+            Assert.assertEquals(firstValue.getOrNull()?.redirectUrl, oneginiAppToWebSingleSignOn.redirectUrl.toString())
+        }
     }
 
     @Test
     fun `When oginini getAppToWebSingleSignOn calls onError on the handler, Then result should fail with the error message and code`() {
         mockParseUri(correctUri)
-        whenCalledWithUrl()
         whenSSOReturnsError()
 
-        getAppToWebSingleSignOnUseCase(callMock, resultMock)
+        getAppToWebSingleSignOnUseCase(correctUri, callbackMock)
 
-        val message = oneginiAppToWebSingleSignOnError.message
-        verify(resultMock).error(eq(oneginiAppToWebSingleSignOnError.errorType.toString()), eq(message), any())
+
+        argumentCaptor<Result<OWAppToWebSingleSignOn>>().apply {
+            verify(callbackMock).invoke(capture())
+            val expected = FlutterError(oneginiAppToWebSingleSignOnError.errorType.toString(), oneginiAppToWebSingleSignOnError.message)
+            SdkErrorAssert.assertEquals(expected, firstValue.exceptionOrNull())
+        }
     }
-
-
 
     private fun mockSingleSignOnObject() {
         whenever(oneginiAppToWebSingleSignOn.token).thenReturn(mockedTokenString)
@@ -122,14 +109,6 @@ class GetAppToWebSingleSignOnUseCaseTests {
 
     private fun mockParseUri(uri: String) {
         whenever(uriFacade.parse(uri)).thenReturn(parsedUri)
-    }
-
-    private fun whenCalledWithUrl() {
-        whenever(callMock.argument<String>("url")).thenReturn(correctUri)
-    }
-
-    private fun whenCalledWithNullUrl() {
-        whenever(callMock.argument<String>("url")).thenReturn(null)
     }
 
 }
