0c891cd6e83d0472c8d85fd2be08893840eb16cd
Michiel Vrins <michiel_678@hotmail.com>
2023-03-27 10:32:37 +0200

    FP-71: Android: Update startUseCase tests

--- android/src/test/java/com/onegini/mobile/sdk/StartAppUseCaseTests.kt
+++ android/src/test/java/com/onegini/mobile/sdk/StartAppUseCaseTests.kt
@@ -1,25 +1,21 @@
 package com.onegini.mobile.sdk
 
-import com.google.common.truth.Truth.assertThat
-import com.google.gson.Gson
 import com.onegini.mobile.sdk.android.client.OneginiClient
 import com.onegini.mobile.sdk.android.handlers.OneginiInitializationHandler
 import com.onegini.mobile.sdk.android.handlers.error.OneginiInitializationError
-import com.onegini.mobile.sdk.android.model.entity.UserProfile
+import com.onegini.mobile.sdk.flutter.OneWelcomeWrapperErrors
 import com.onegini.mobile.sdk.flutter.OneginiSDK
-import com.onegini.mobile.sdk.flutter.models.Config
-import com.onegini.mobile.sdk.flutter.models.CustomIdentityProviderConfig
+import com.onegini.mobile.sdk.flutter.SdkErrorAssert
+import com.onegini.mobile.sdk.flutter.helpers.SdkError
+import com.onegini.mobile.sdk.flutter.pigeonPlugin.FlutterError
 import com.onegini.mobile.sdk.flutter.useCases.StartAppUseCase
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Answers
 import org.mockito.Mock
-import org.mockito.Spy
 import org.mockito.junit.MockitoJUnitRunner
-
 import org.mockito.kotlin.*
 
 @RunWith(MockitoJUnitRunner::class)
@@ -32,160 +28,101 @@ class StartAppUseCaseTests {
     lateinit var clientMock: OneginiClient
 
     @Mock
-    lateinit var callMock: MethodCall
-
-    @Spy
-    lateinit var resultSpy: MethodChannel.Result
+    lateinit var callbackMock: (Result<Unit>) -> Unit
 
     @Mock
     lateinit var oneginiInitializationError: OneginiInitializationError
 
     lateinit var startAppUseCase: StartAppUseCase
+
     @Before
     fun attach() {
         startAppUseCase = StartAppUseCase(oneginiSdk)
     }
 
+    val errorCode = OneginiInitializationError.GENERAL_ERROR
+    val errorMessage = "General error"
+
     @Test
-    fun `should return UserProfile when start method returns single UserProfile`() {
-        whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
-            it.getArgument<OneginiInitializationHandler>(0).onSuccess(setOf(UserProfile("QWERTY")))
+    fun `When onError gets called by oneginiClient, Then should call callback with Result_failure with that error`() {
+
+        whenCallsOnError()
+
+
+        startAppUseCase(
+            null,
+            null,
+            null,
+            null,
+            null,
+            callbackMock)
+
+        argumentCaptor<Result<Unit>>().apply {
+            verify(callbackMock).invoke(capture())
+            val expected = FlutterError(oneginiInitializationError.errorType.toString(), oneginiInitializationError.message)
+            SdkErrorAssert.assertEquals(expected, firstValue.exceptionOrNull())
         }
-
-        startAppUseCase(callMock, resultSpy)
-
-        val expectedList = setOf(mapOf("isDefault" to false, "profileId" to "QWERTY"))
-        val expectedResult = Gson().toJson(expectedList)
-        verify(resultSpy).success(expectedResult)
     }
 
     @Test
-    fun `should return error when when start method returns error`() {
+    fun `When onSuccess gets called by oneginiClient, Then should call callback with Result_success`() {
+
+        whenCallsOnSuccess()
+
+
+        startAppUseCase(
+            null,
+            null,
+            null,
+            null,
+            null,
+            callbackMock)
+
+        argumentCaptor<Result<Unit>>().apply {
+            verify(callbackMock).invoke(capture())
+            assertEquals(firstValue.isSuccess, true)
+        }
+    }
+
+    @Test
+    fun `When OneginiSDK_buildSDK throws an exception Then we should reject with that error`() {
+        whenBuildSdkThrowsSdkError()
+
+        startAppUseCase(
+            null,
+            null,
+            null,
+            null,
+            null,
+            callbackMock)
+
+        argumentCaptor<Result<Unit>>().apply {
+            verify(callbackMock).invoke(capture())
+            val expected = FlutterError(errorCode.toString(), errorMessage)
+            SdkErrorAssert.assertEquals(expected, firstValue.exceptionOrNull())
+        }
+    }
+
+    private fun whenCallsOnError() {
         whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
             it.getArgument<OneginiInitializationHandler>(0).onError(oneginiInitializationError)
         }
-        whenever(oneginiInitializationError.errorType).thenReturn(OneginiInitializationError.GENERAL_ERROR)
-        whenever(oneginiInitializationError.message).thenReturn("General error")
-
-        startAppUseCase(callMock, resultSpy)
-
-        val message = oneginiInitializationError.message
-        verify(resultSpy).error(eq(oneginiInitializationError.errorType.toString()), eq(message), any())
+        whenever(oneginiInitializationError.errorType).thenReturn(errorCode)
+        whenever(oneginiInitializationError.message).thenReturn(errorMessage)
     }
 
-    @Test
-    fun `should return set of UserProfiles when start method returns set with UserProfiles`() {
-        whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
-            it.getArgument<OneginiInitializationHandler>(0).onSuccess(setOf(UserProfile("QWERTY"), UserProfile("ASDFGH")))
+    private fun whenBuildSdkThrowsSdkError() {
+        whenever(oneginiSdk.buildSDK(anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull(), anyOrNull())).thenAnswer {
+            throw SdkError(
+                code = errorCode,
+                message = errorMessage
+            )
         }
-
-        startAppUseCase(callMock, resultSpy)
-
-        val expectedList = setOf(mapOf("isDefault" to false, "profileId" to "QWERTY"), mapOf("isDefault" to false, "profileId" to "ASDFGH"))
-        val expectedResult = Gson().toJson(expectedList)
-        verify(resultSpy).success(expectedResult)
     }
 
-    @Test
-    fun `should return empty set when start method returns empty set`() {
+    private fun whenCallsOnSuccess() {
         whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
             it.getArgument<OneginiInitializationHandler>(0).onSuccess(emptySet())
-        }
-
-        startAppUseCase(callMock, resultSpy)
-
-        val expectedList = emptySet<UserProfile>()
-        val expectedResult = Gson().toJson(expectedList)
-        verify(resultSpy).success(expectedResult)
-    }
-
-    @Test
-    fun `should properly pass customIdentityProviderConfigs JSON string list within the config to the SDK when list contains two items`() {
-        whenever(callMock.argument<ArrayList<String>>("customIdentityProviderConfigs"))
-            .thenReturn(arrayListOf(Gson().toJson(CustomIdentityProviderConfig("id1", false)),
-                Gson().toJson(CustomIdentityProviderConfig("id2", true))))
-        whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
-            it.getArgument<OneginiInitializationHandler>(0).onSuccess(emptySet())
-        }
-
-        startAppUseCase(callMock, resultSpy)
-
-        argumentCaptor<Config> {
-            verify(oneginiSdk).buildSDK(capture(), eq(resultSpy))
-            assertThat(firstValue.customIdentityProviderConfigs.size).isEqualTo(2)
-            assertThat(firstValue.customIdentityProviderConfigs[0].providerId).isEqualTo("id1")
-            assertThat(firstValue.customIdentityProviderConfigs[0].isTwoStep).isEqualTo(false)
-            assertThat(firstValue.customIdentityProviderConfigs[1].providerId).isEqualTo("id2")
-            assertThat(firstValue.customIdentityProviderConfigs[1].isTwoStep).isEqualTo(true)
-        }
-    }
-
-    @Test
-    fun `should properly pass customIdentityProviderConfigs JSON string list within the config to the SDK when list contains one item`() {
-        whenever(callMock.argument<ArrayList<String>>("customIdentityProviderConfigs"))
-            .thenReturn(arrayListOf(Gson().toJson(CustomIdentityProviderConfig("id1", false))))
-        whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
-            it.getArgument<OneginiInitializationHandler>(0).onSuccess(emptySet())
-        }
-
-        startAppUseCase(callMock, resultSpy)
-
-        argumentCaptor<Config> {
-            verify(oneginiSdk).buildSDK(capture(), eq(resultSpy))
-            assertThat(firstValue.customIdentityProviderConfigs.size).isEqualTo(1)
-            assertThat(firstValue.customIdentityProviderConfigs[0].providerId).isEqualTo("id1")
-            assertThat(firstValue.customIdentityProviderConfigs[0].isTwoStep).isEqualTo(false)
-        }
-    }
-    
-    @Test
-    fun `should properly pass connectionTimeout and readTimeout params to the SDK when provided`() {
-        whenever(callMock.argument<Int>("connectionTimeout")).thenReturn(5)
-        whenever(callMock.argument<Int>("readTimeout")).thenReturn(20)
-        whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
-            it.getArgument<OneginiInitializationHandler>(0).onSuccess(emptySet())
-        }
-
-        startAppUseCase(callMock, resultSpy)
-
-        argumentCaptor<Config> {
-            verify(oneginiSdk).buildSDK(capture(), eq(resultSpy))
-            assertThat(firstValue.httpConnectionTimeout).isEqualTo(5)
-            assertThat(firstValue.httpReadTimeout).isEqualTo(20)
-        }
-    }
-
-    @Test
-    fun `should properly pass securityControllerClassName and configModelClassName params to the SDK when provided`() {
-        whenever(callMock.argument<String>("securityControllerClassName")).thenReturn("com.onegini.mobile.onegini_example.SecurityController")
-        whenever(callMock.argument<String>("configModelClassName")).thenReturn("com.onegini.mobile.onegini_example.OneginiConfigModel")
-        whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
-            it.getArgument<OneginiInitializationHandler>(0).onSuccess(emptySet())
-        }
-
-        startAppUseCase(callMock, resultSpy)
-
-        argumentCaptor<Config> {
-            verify(oneginiSdk).buildSDK(capture(), eq(resultSpy))
-            assertThat(firstValue.configModelClassName).isEqualTo("com.onegini.mobile.onegini_example.OneginiConfigModel")
-            assertThat(firstValue.securityControllerClassName).isEqualTo("com.onegini.mobile.onegini_example.SecurityController")
-        }
-    }
-
-    @Test
-    fun `should properly pass connectionTimeout and readTimeout params with zero values to the SDK when provided`() {
-        whenever(callMock.argument<Int>("connectionTimeout")).thenReturn(0)
-        whenever(callMock.argument<Int>("readTimeout")).thenReturn(0)
-        whenever(oneginiSdk.oneginiClient.start(any())).thenAnswer {
-            it.getArgument<OneginiInitializationHandler>(0).onSuccess(emptySet())
-        }
-
-        startAppUseCase(callMock, resultSpy)
-
-        argumentCaptor<Config> {
-            verify(oneginiSdk).buildSDK(capture(), eq(resultSpy))
-            assertThat(firstValue.httpConnectionTimeout).isEqualTo(0)
-            assertThat(firstValue.httpReadTimeout).isEqualTo(0)
         }
     }
 }
