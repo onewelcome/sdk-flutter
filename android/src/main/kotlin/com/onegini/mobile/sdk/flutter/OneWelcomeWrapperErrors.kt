a2a1810fbb1c1528cd08c9c231906ff70168f5e6
Michiel Vrins <michiel_678@hotmail.com>
2023-03-29 11:42:31 +0200

    FP-57: Rename browser-authentication error to browser Registration

--- android/src/main/kotlin/com/onegini/mobile/sdk/flutter/OneWelcomeWrapperErrors.kt
+++ android/src/main/kotlin/com/onegini/mobile/sdk/flutter/OneWelcomeWrapperErrors.kt
@@ -12,7 +12,7 @@ enum class OneWelcomeWrapperErrors(val code: Int, val message: String) {
     AUTHENTICATION_NOT_IN_PROGRESS(8037, "Authentication is currently not in progress"),
     FINGERPRINT_AUTHENTICATION_NOT_IN_PROGRESS(8038, "Fingerprint Authentication is currently not in progress"),
     OTP_AUTHENTICATION_NOT_IN_PROGRESS(8039, "OTP Authentication is currently not in progress"),
-    BROWSER_AUTHENTICATION_NOT_IN_PROGRESS(8040, "Browser Authentication is currently not in progress"),
+    BROWSER_REGISTRATION_NOT_IN_PROGRESS(8040, "Browser registration is currently not in progress"),
 
     // Errors that only occur on Android
     IDENTITY_PROVIDER_NOT_FOUND(8005, "The requested identity provider is not found"),
