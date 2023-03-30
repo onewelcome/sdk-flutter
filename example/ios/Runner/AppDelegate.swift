60e4229526906a32e471dd933a638a4eb6d12f42
Michiel Vrins <michiel_678@hotmail.com>
2023-03-30 16:00:05 +0200

    FP-32: Lint AppDelegate.swift and remove unused code

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,34 +1,33 @@
 import UIKit
+
 import Flutter
+
 import onegini
+
 import OneginiSDKiOS
 
 @UIApplicationMain
+
 @objc class AppDelegate: FlutterAppDelegate {
-    let exampleCustomEventIdentifier: String = "exemple_events"
-    
     override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+
+    _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+
     ) -> Bool {
-
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-
-        let methodChannel = FlutterMethodChannel(name: "example",
-                                                     binaryMessenger: controller.binaryMessenger)
-
-        let eventChannel = FlutterEventChannel(name: exampleCustomEventIdentifier,
-                                                  binaryMessenger: controller.binaryMessenger)
-
         GeneratedPluginRegistrant.register(with: self)
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
     }
-    
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        
+
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+
         let isOneginiUrlCallback: Bool = OneginiModuleSwift.sharedInstance.handleDeepLinkCallbackUrl(url)
+
         debugPrint(isOneginiUrlCallback)
-        
+
         return true
+
       }
+
 }
