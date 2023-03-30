2a77ac90ce027f0745b631720df5ed8b0073b1bc
Michiel Vrins <michiel_678@hotmail.com>
2023-03-30 13:35:39 +0200

    FP-71: Comment styling change

--- android/src/main/kotlin/com/onegini/mobile/sdk/flutter/constants/Constants.kt
+++ android/src/main/kotlin/com/onegini/mobile/sdk/flutter/constants/Constants.kt
@@ -2,7 +2,7 @@ package com.onegini.mobile.sdk.flutter.constants
 
 interface Constants {
     companion object {
-        /** HTTP Response properties */
+        // HTTP Response properties
         const val RESPONSE_STATUS_CODE = "statusCode"
         const val RESPONSE_HEADERS = "headers"
         const val RESPONSE_BODY = "body"
