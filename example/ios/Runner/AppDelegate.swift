8b9f275aaf3b8951c39f333de285326b368b33fd
Michiel Vrins <michiel_678@hotmail.com>
2023-03-31 11:15:30 +0200

    FP-32: Remove empty lines added because of wrong line endings
    This file had CRLF endings instead of LF and SwiftLint converts this to
    LF but will add extra newlines by accident. This commit removes those
    newlines.

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,33 +1,20 @@
 import UIKit
-
 import Flutter
-
 import onegini
-
 import OneginiSDKiOS
 
 @UIApplicationMain
-
 @objc class AppDelegate: FlutterAppDelegate {
     override func application(
-
     _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
-
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-
     }
 
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-
         let isOneginiUrlCallback: Bool = OneginiModuleSwift.sharedInstance.handleDeepLinkCallbackUrl(url)
-
         debugPrint(isOneginiUrlCallback)
-
         return true
-
       }
-
 }
