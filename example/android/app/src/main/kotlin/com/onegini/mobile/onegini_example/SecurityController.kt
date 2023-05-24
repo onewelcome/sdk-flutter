f9434f9f78e68892fc7224eb1d145454dc9b80ac
Michiel Vrins <michiel_678@hotmail.com>
2023-05-24 16:22:37 +0200

    FP-89: Use new 'access' tokenserver

--- example/android/app/src/main/kotlin/com/onegini/mobile/onegini_example/SecurityController.kt
+++
@@ -1,7 +0,0 @@
-package com.onegini.mobile.flutterExample
-
-object SecurityController {
-    const val debugDetection = false
-    const val rootDetection = false
-    const val debugLogs = true
-}
